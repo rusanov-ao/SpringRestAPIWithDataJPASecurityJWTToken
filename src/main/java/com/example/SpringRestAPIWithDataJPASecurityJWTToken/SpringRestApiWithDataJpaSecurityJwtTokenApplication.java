b4b0355dc7991a154ce96ac88dd29d87e2c82794
alex <rusanov-ao@inbox.ru>
2026-02-19 23:30:27 +0300

    Initial commit

---
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/SpringRestApiWithDataJpaSecurityJwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRestAPIWithDataJPASecurityJWTToken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestApiWithDataJpaSecurityJwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestApiWithDataJpaSecurityJwtTokenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/models/Person.java
@@ -0,0 +1,83 @@
+package com.example.SpringRestAPIWithDataJPASecurityJWTToken.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "year_of_birth")
+    private int yearOfBirth;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+    public Person() {
+    }
+
+    public Person(String username, int yearOfBirth) {
+        this.username = username;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
