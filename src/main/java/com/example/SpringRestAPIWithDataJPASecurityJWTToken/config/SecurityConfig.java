2fee3af620e0017eef99b02edc06ce8ee3ad05ad
alex <rusanov-ao@inbox.ru>
2026-02-20 22:05:43 +0300

    up

---
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.SpringRestAPIWithDataJPASecurityJWTToken.config;
+
+import com.example.SpringRestAPIWithDataJPASecurityJWTToken.services.PersonDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    private final PersonDetailService personDetailService;
+
+    public SecurityConfig(PersonDetailService personDetailService) {
+        this.personDetailService = personDetailService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
