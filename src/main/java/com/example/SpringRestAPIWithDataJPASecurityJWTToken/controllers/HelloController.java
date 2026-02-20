8ba3c3668e49ab930c3712de52a50b80fc274749
alex <rusanov-ao@inbox.ru>
2026-02-20 22:32:13 +0300

    up

---
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/controllers/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.SpringRestAPIWithDataJPASecurityJWTToken.controllers;
+
+import com.example.SpringRestAPIWithDataJPASecurityJWTToken.security.PersonDetails;
+import com.example.SpringRestAPIWithDataJPASecurityJWTToken.services.AdminService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    private final AdminService adminService;
+
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+
+        return personDetails.getUsername();
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        adminService.doAdminStuff();
+        return "admin";
+    }
+}
