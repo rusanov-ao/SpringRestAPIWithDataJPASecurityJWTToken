617b061da7b81ce5e5aabffcb064884e6223d3c7
alex <rusanov-ao@inbox.ru>
2026-02-20 22:07:18 +0300

    up

--- src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/SpringRestApiWithDataJpaSecurityJwtTokenApplication.java
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/SpringRestApiWithDataJpaSecurityJwtTokenApplication.java
@@ -1,13 +1,20 @@
 package com.example.SpringRestAPIWithDataJPASecurityJWTToken;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRestApiWithDataJpaSecurityJwtTokenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiWithDataJpaSecurityJwtTokenApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
