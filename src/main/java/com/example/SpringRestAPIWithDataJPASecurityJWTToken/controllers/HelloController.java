157086ed64081db978ab349ae10585611893cd11
alex <rusanov-ao@inbox.ru>
2026-02-22 22:50:35 +0300

    up

--- src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/controllers/HelloController.java
+++ src/main/java/com/example/SpringRestAPIWithDataJPASecurityJWTToken/controllers/HelloController.java
@@ -7,8 +7,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     private final AdminService adminService;
@@ -34,6 +35,6 @@ public class HelloController {
     @GetMapping("/admin")
     public String adminPage() {
         adminService.doAdminStuff();
-        return "admin";
+        return "Страница админа";
     }
 }
